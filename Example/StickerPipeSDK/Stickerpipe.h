e38cef9a294a40fbe5d68f14c5228e1c20f68b16
Kirill Klebanov <headmast@bk.ru>
2019-11-27 09:35:45 +0300

    Rename framework to Stickerpipe

---
+++ Example/StickerPipeSDK/Stickerpipe.h
@@ -0,0 +1,26 @@
+//
+//  Stickerpipe.h
+//  Stickerpipe
+//
+//  Created by Kirill Klebanov on 11/22/19.
+//  Copyright © 2019 908 Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Stickerpipe.
+FOUNDATION_EXPORT double StickerpipeVersionNumber;
+
+//! Project version string for Stickerpipe.
+FOUNDATION_EXPORT const unsigned char StickerpipeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stickerpipe/PublicHeader.h>
+
+#import <Stickerpipe/STKStickerController.h>
+#import <Stickerpipe/STKStickersConstants.h>
+#import <Stickerpipe/STKStickersManager.h>
+#import <Stickerpipe/STKImageManager.h>
+#import <Stickerpipe/STKShowStickerButton.h>
+#import <Stickerpipe/UIImageView+Stickers.h>
+#import <Stickerpipe/STKStickerHeaderDelegateManager.h>
+#import <Stickerpipe/UIView+CordsAdditions.h>
