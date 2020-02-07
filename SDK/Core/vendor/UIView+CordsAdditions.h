96647c7e4c282599ef220112e22c9e95dbe15f2e
Kirill <kirill.k@just.ru.com>
2020-02-07 10:27:32 +0300

    Delete unused cords additions

--- SDK/Core/vendor/UIView+CordsAdditions.h
+++ SDK/Core/vendor/UIView+CordsAdditions.h
@@ -8,15 +8,15 @@
 
 @interface UIView (CordsAdditions)
 
-@property (nonatomic) CGFloat width;
-@property (nonatomic) CGFloat height;
-@property (nonatomic) CGFloat left;
-@property (nonatomic) CGFloat right;
-@property (nonatomic) CGFloat top;
-@property (nonatomic) CGFloat bottom;
-@property (nonatomic) CGFloat centerX;
-@property (nonatomic) CGFloat centerY;
-@property (nonatomic) CGPoint origin;
-@property (nonatomic) CGSize size;
+//@property (nonatomic) CGFloat width;
+//@property (nonatomic) CGFloat height;
+//@property (nonatomic) CGFloat left;
+//@property (nonatomic) CGFloat right;
+//@property (nonatomic) CGFloat top;
+//@property (nonatomic) CGFloat bottom;
+//@property (nonatomic) CGFloat centerX;
+//@property (nonatomic) CGFloat centerY;
+//@property (nonatomic) CGPoint origin;
+//@property (nonatomic) CGSize size;
 
 @end
